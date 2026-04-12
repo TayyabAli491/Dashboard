372143fb5c0544867c690264efc9f9276c9e6a9a
TayyabAli491 <alihaniff5@gmail.com>
2026-04-12 16:35:38 +0500

    Add database tables for payload schemas, sessions, telemetry records, and workspaces with appropriate foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_08_045925) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_11_102403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "payload_schemas", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.jsonb "fields", default: [], null: false
+    t.datetime "updated_at", null: false
+    t.bigint "workspace_id", null: false
+    t.index ["workspace_id"], name: "index_payload_schemas_on_workspace_id", unique: true
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.datetime "ended_at"
+    t.string "name", null: false
+    t.datetime "started_at"
+    t.datetime "updated_at", null: false
+    t.bigint "workspace_id", null: false
+    t.index ["workspace_id"], name: "index_sessions_on_workspace_id"
+  end
+
+  create_table "telemetry_records", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.jsonb "processed_payload", default: {}
+    t.jsonb "raw_payload", default: {}, null: false
+    t.datetime "recorded_at", null: false
+    t.bigint "session_id"
+    t.datetime "updated_at", null: false
+    t.bigint "workspace_id", null: false
+    t.index ["recorded_at"], name: "index_telemetry_records_on_recorded_at"
+    t.index ["session_id"], name: "index_telemetry_records_on_session_id"
+    t.index ["workspace_id"], name: "index_telemetry_records_on_workspace_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,4 +57,22 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_08_045925) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  create_table "workspaces", force: :cascade do |t|
+    t.string "api_key", null: false
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.boolean "is_public", default: false, null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["api_key"], name: "index_workspaces_on_api_key", unique: true
+    t.index ["user_id"], name: "index_workspaces_on_user_id"
+  end
+
+  add_foreign_key "payload_schemas", "workspaces"
+  add_foreign_key "sessions", "workspaces"
+  add_foreign_key "telemetry_records", "sessions"
+  add_foreign_key "telemetry_records", "workspaces"
+  add_foreign_key "workspaces", "users"
 end
