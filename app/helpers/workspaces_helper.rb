1cf27dda6812dd1ed69b9d7fbd0279a2a416b574
TayyabAli491 <alihaniff5@gmail.com>
2026-04-18 14:06:20 +0500

    Add helper method to generate example JSON payload from schema

--- app/helpers/workspaces_helper.rb
+++ app/helpers/workspaces_helper.rb
@@ -1,2 +1,21 @@
 module WorkspacesHelper
+  def example_payload_json(payload_schema)
+    return if payload_schema.blank?
+
+    example = payload_schema.fields.each_with_object({}) do |field, hash|
+      hash[field["key"]] = example_value_for_type(field["type"])
+    end
+    JSON.pretty_generate(example)
+  end
+
+  private
+
+  def example_value_for_type(field_type)
+    case field_type
+    when "float"   then 0.0
+    when "integer" then 0
+    when "boolean" then false
+    when "string"  then ""
+    end
+  end
 end
