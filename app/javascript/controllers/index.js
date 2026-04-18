a3127476971623509955f78012b2a2617ac19387
TayyabAli491 <alihaniff5@gmail.com>
2026-04-18 14:51:03 +0500

    Register AutoRefreshController only if not already registered

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,13 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import AutoRefreshController from "controllers/auto_refresh_controller"
+
+unlessAutoRefreshRegistered()
 
 eagerLoadControllersFrom("controllers", application)
+
+function unlessAutoRefreshRegistered() {
+  if (application.router.modulesByIdentifier.has("auto-refresh")) return
+
+  application.register("auto-refresh", AutoRefreshController)
+}
